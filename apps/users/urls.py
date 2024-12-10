4683b1b2a3029b83781f42bc1698e20d698405bf
Boris Popov <32306686+bpopovgit@users.noreply.github.com>
2024-12-10 14:05:17 +0200

    feat: added workout_log_form.html and workoutlog_list.html

--- apps/users/urls.py
+++ apps/users/urls.py
@@ -7,4 +7,5 @@ urlpatterns = [
     path('signup/', SignUpView.as_view(), name='signup'),
     path('profile/', profile_view, name='profile'),
     path('profile/update/', UserProfileUpdateView.as_view(), name='profile_update'),
+
 ]
