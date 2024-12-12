80c511d9419b6666f10df010e39d7ebc2d6e4b4c
Boris Popov <32306686+bpopovgit@users.noreply.github.com>
2024-12-12 16:42:13 +0200

    feat: added custom css files for the schedule and base, added additional styling to the schedule, added new and improved styling to http://127.0.0.1:8000/workouts/ and its relevant forms

--- apps/workouts/forms.py
+++ apps/workouts/forms.py
@@ -1,3 +1,5 @@
+from crispy_forms.helper import FormHelper
+from crispy_forms.layout import Submit
 from django import forms
 from django_select2.forms import Select2MultipleWidget
 
@@ -8,7 +10,10 @@ class WorkoutForm(forms.ModelForm):
     class Meta:
         model = Workout
         fields = ['name', 'exercises', 'duration', 'intensity']
-        widgets = {
-            'exercises': Select2MultipleWidget,  # Use Select2 for the exercises field
-        }
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.helper = FormHelper()
+        self.helper.form_method = 'post'
+        self.helper.add_input(Submit('submit', 'Save Workout', css_class='btn-primary'))
 
