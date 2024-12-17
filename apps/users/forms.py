91dfccec56cfbf86f7f31cb3f3d1510a4ba4c9eb
Boris Popov <32306686+bpopovgit@users.noreply.github.com>
2024-12-17 11:08:01 +0200

    fix: updated the styling of the sign up form to have a proper modern look.

--- apps/users/forms.py
+++ apps/users/forms.py
@@ -1,3 +1,4 @@
+import random
 from django import forms
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User
@@ -10,3 +11,22 @@ class CustomUserCreationForm(UserCreationForm):
     class Meta:
         model = User
         fields = ['username', 'first_name', 'last_name', 'email', 'password1', 'password2']
+        labels = {
+            'password2': 'Repeat Password',  # Rename the field label
+        }
+
+    def clean_password1(self):
+        password1 = self.cleaned_data.get('password1')
+        errors = []
+
+        # Custom password requirements
+        if len(password1) < 8:
+            errors.append("Your password must contain at least 8 characters.")
+        if password1.isnumeric():
+            errors.append("Your password cannot be entirely numeric.")
+        if password1.lower() in ['password', '12345678']:
+            errors.append("Your password cannot be a commonly used password.")
+
+        if errors:
+            raise forms.ValidationError(random.choice(errors))  # Display a random error
+        return password1
